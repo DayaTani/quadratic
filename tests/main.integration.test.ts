bf04bdefd7c3b4a1409696475574992e8f3353bb
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2024-03-04 16:02:01 +0700

    test: add more integration test cases

--- tests/main.integration.test.ts
+++ tests/main.integration.test.ts
@@ -6,6 +6,7 @@ describe('quadratic CLI tool', () => {
   it.each([
     [['1', '-5', '6'], '3\t2'],
     [['2', '3', '-5'], '1\t-2.5'],
+    [['2', '4', '5'], ''],
     [['1', '6', '9'], '-3\t-3'],
   ])('finds roots of quadratic quations', (restArgs, expected) => {
     // Prepare
@@ -19,6 +20,5 @@ describe('quadratic CLI tool', () => {
     // Assert
     expect(outPrinter).toHaveBeenCalledTimes(1)
     expect(outPrinter).toHaveBeenCalledWith(expected)
-
   })
 })
